2884fcd7b1aa66b9e3a4b1f51e0fd20f820d5f96
yingwang54 <85709022+yingwang54@users.noreply.github.com>
2022-07-13 18:34:50 +0200

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">       
+    <div className="App">  
+    <div className="container">   
         <p>
-          Edit 
-           and save to reload.
+          This project is coded by <a href="https://www.linkedin.com/in/ying-wang-00399620b/" target="_blank" rel="noreferrer">Ying Wang</a> and open source in <a href="https://github.com/yingwang54/react-weather-app" target="_blank" rel="noreferrer">Github</a>.
         </p>
-    </div>
+    </div></div> 
   );
 }
